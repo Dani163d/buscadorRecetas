f94632a12e36dec8a7ff11dfa2fa6d49defd01c0
Dani163d <Danielasilvacastillo163@gmail.com>
2024-09-22 15:00:15 -0400

    obtener platillos de la categoria seleccionada

--- js/app.js
+++ js/app.js
@@ -25,7 +25,9 @@ function iniciarApp() {
     function selectCategoria(e) {
         const categoria = e.target.value;
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
-        console.log(url);
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then( resultado => console.log(resultado));
     }
 }
 
