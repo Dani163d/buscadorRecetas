5f935598834d37fca8db21db3678b5aeecb3d5d3
Dani163d <Danielasilvacastillo163@gmail.com>
2024-09-27 20:44:10 -0400

    agregando botones de favorito y cerrar

--- js/app.js
+++ js/app.js
@@ -127,8 +127,27 @@ function iniciarApp() {
 
         modalBody.appendChild(listGroup);
 
-        // muestra el modal
+        const modalFooter = document.querySelector('.modal-footer');
+        limpiarHtml(modalFooter);
+
+        // Botones de cerrar y favorito
+        const btnFavorito = document.createElement('BUTTON');
+        btnFavorito.classList.add('btn', 'btn-danger', 'col');
+        btnFavorito.textContent ='Guardar Favorito';
+
+        const btnCerrarModal = document.createElement('BUTTON');
+        btnCerrarModal.classList.add('btn', 'btn-secondary', 'col');
+        btnCerrarModal.textContent = 'Cerrar';
+        btnCerrarModal.onclick = function() {
+            modal.hide();
+        }
+
+        modalFooter.appendChild(btnFavorito);
+        modalFooter.appendChild(btnCerrarModal);
+
+        // Muestra el modal
         modal.show();
+
     }
 
     function limpiarHtml(selector) {
